371625515dbcab8e1e8356236ee245572ad8a36b
Yrrsdh <149466233+Yrrsdh@users.noreply.github.com>
2023-11-06 00:25:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,14 +6,14 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 
-global.owner = [
-  ['919074692450', 'ABHISHEK-SURESH', true],
+global.owner = [unnikuttan]
+  ['919747545952', 'unnikuttan', true],
   [''],
   [''] 
-] //Number of owners
+] //Number919747545952 of owners
 
-global.mods = ['919074692450','919074692450'] 
-global.prems = ['919074692450', '919074692450', '919074692450']
+global.mods = ['919747545952','919747545952'] 
+global.prems = ['919747545952', '919747545952', '919747545952']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -47,11 +47,11 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = '𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝙴𝚁'
+global.botname = 'unnikuttan'
 global.premium = 'true'
-global.packname = '𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝙴𝚁🎯𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝚄𝚁𝙴𝚂𝙷☘️' 
-global.author = 'ABHISHEK SURESH' 
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/abhishek_ser\n' 
+global.packname = '�unnikuttan❤️unnikuttan☘️' 
+global.author = 'unnikuttan' 
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/unnikuttan\n' 
 global.dygp = 'https://chat.whatsapp.com/BOLb0ICN3sAJ5dloRBw5VD'
 global.fgsc = 'https://github.com/AbhishekSuresh2/ABHISHEK-SER' 
 global.fgyt = 'https://youtube.com/@comedymelodych8468'
